bd857e7cac1792baf2cc703d093f8a62127575e5
liza <liza.kosenuk@yandex.ru>
2019-11-30 15:18:27 +0300

    first change

--- codkd.py
+++ codkd.py
@@ -4,7 +4,10 @@ def my_superfunction():
 
 def main():
     print('My first git program')
-
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
 
 if __name__ == '__main__':
     main()
